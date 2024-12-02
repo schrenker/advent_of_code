977138f7368b146f009fc64ed63319caf865332d
Sebastian Zawadzki <sebastian@zawadzki.tech>
2024-12-02 13:06:27 +0100

    refactor d02

--- challenge/y2024/d02.py
+++ challenge/y2024/d02.py
@@ -5,34 +5,23 @@ def check_safe(report):
     increasing = True if report[0] < report[1] else False
 
     for i in range(0, len(report) - 1):
-        if (report[i] < report[i + 1]) is not increasing:
-            return False
-        if not 1 <= abs(report[i] - report[i + 1]) <= 3:
-            return False
-    return True
-
-
-def check_safe_with_index(report):
-    increasing = True if report[0] < report[1] else False
-
-    for i in range(0, len(report) - 1):
-        if (report[i] < report[i + 1]) is not increasing:
-            return False, i, i + 1
-        if not 1 <= abs(report[i] - report[i + 1]) <= 3:
+        if ((report[i] < report[i + 1]) is not increasing) or (
+            not 1 <= abs(report[i] - report[i + 1]) <= 3
+        ):
             return False, i, i + 1
     return True, -1, -1
 
 
 def check_safe_with_dampener(report):
-    result, idx1, idx2 = check_safe_with_index(report)
+    result, idx1, idx2 = check_safe(report)
     if result is True:
         return result
     else:
         return (
-            check_safe_with_index(report[:0] + report[1:])[0]
-            or check_safe_with_index(report[:1] + report[2:])[0]
-            or check_safe_with_index(report[:idx1] + report[idx1 + 1 :])[0]
-            or check_safe_with_index(report[:idx2] + report[idx2 + 1 :])[0]
+            check_safe(report[:0] + report[1:])[0]
+            or check_safe(report[:1] + report[2:])[0]
+            or check_safe(report[:idx1] + report[idx1 + 1 :])[0]
+            or check_safe(report[:idx2] + report[idx2 + 1 :])[0]
         )
 
 
@@ -40,7 +29,7 @@ def part_one(input):
     acc = 0
     for i in input.splitlines():
         report = [int(x) for x in i.split()]
-        if check_safe(report):
+        if check_safe(report)[0]:
             acc += 1
     return acc
 
