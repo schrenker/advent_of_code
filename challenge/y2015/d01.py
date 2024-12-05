324508efddddb992bf726277122ff069a04c0807
Sebastian Zawadzki <sebastian@zawadzki.tech>
2024-12-05 15:33:34 +0100

    day one of 2015

---
+++ challenge/y2015/d01.py
@@ -0,0 +1,31 @@
+test_results = {
+    "part_one": {
+        "1": 0,
+        "2": 0,
+        "3": 3,
+        "4": 3,
+        "5": 3,
+        "6": -1,
+        "7": -1,
+        "8": -3,
+        "9": -3,
+    },
+    "part_two": {"1": 1, "2": 5},
+}
+
+
+def part_one(input):
+    floor = 0
+    for i in input.rstrip():
+        floor = floor + 1 if i == "(" else floor + -1
+    return floor
+
+
+def part_two(input):
+    floor = 0
+    for i in range(len(input)):
+        floor = floor + 1 if input[i] == "(" else floor + -1
+
+        if floor == -1:
+            return i + 1
+    return None
